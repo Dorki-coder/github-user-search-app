bd02055a3acd975c737346d65b907ab2a71283ef
Dorki <bartdane@yandex.ru>
2022-09-19 16:14:28 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style/App.css";
+import { createContext, useState } from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Card from "./components/Card";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+  const [data, setData] = useState({});
+  const [isLoading, setLoading] = useState(false);
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <div className="container">
+          <Header theme={theme} toggleTheme={toggleTheme}></Header>
+          <Search setLoading={setLoading} setData={setData}></Search>
+          <Card theme={theme} isLoading={isLoading} data={data}></Card>
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
